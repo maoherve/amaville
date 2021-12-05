31514a45a444e02af10177bf4988e1ed4cc016cd
Mao Mater <mao@MacBook-Pro-de-Mao.home>
2021-12-05 06:33:21 +0100

    regis and log

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -29,10 +29,6 @@ class RegistrationFormType extends AbstractType
                 ],
             ])
 
-
-
-
-
             ->add('plainPassword',RepeatedType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
