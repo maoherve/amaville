79a86e645a7ef777493474bf83f969e1c0eff025
Mao Mater <mao@MacBook-Pro-de-Mao.home>
2021-12-14 02:11:24 +0100

    finish ads et ajout footer

--- src/Controller/AnnoncesController.php
+++ src/Controller/AnnoncesController.php
@@ -7,13 +7,16 @@ use App\Entity\User;
 use App\Form\AnnoncesType;
 use App\Repository\AnnoncesRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\User\UserInterface;
 
-
+/**
+ * @IsGranted("ROLE_USER")
+ */
 #[Route('/annonces')]
 class AnnoncesController extends AbstractController
 {
