4abe505fcbb40eb2344f2fa64ad5da494753611c
Mao Mater <mao@MacBook-Pro-de-Mao.home>
2021-12-20 00:46:45 +0100

    add config heroku

--- src/Entity/Utilisateur.php
+++ src/Entity/Utilisateur.php
@@ -14,7 +14,7 @@ use Symfony\Component\Security\Core\User\UserInterface;
  * @ORM\Entity(repositoryClass=UserRepository::class)
  * @UniqueEntity(fields={"email"}, message="There is already an account with this email")
  */
-class User implements UserInterface, PasswordAuthenticatedUserInterface
+class Utilisateur implements UserInterface, PasswordAuthenticatedUserInterface
 {
     /**
      * @ORM\Id
